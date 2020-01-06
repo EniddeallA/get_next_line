2ba4d3b3a8c0bb3c5faf5550b389265c1b76a234
Allaeddine Khalid <akhalid@e1r12p10.1337.ma>
2020-01-06 07:37:24 +0100

    mandatory and bonus parts

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: akhalid <akhalid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 01:48:20 by akhalid           #+#    #+#             */
-/*   Updated: 2019/12/27 22:10:47 by akhalid          ###   ########.fr       */
+/*   Updated: 2019/11/02 21:57:01 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_read(int fd, char **str)
+char	*ft_read(int fd, char *str)
 {
 	char	*buff;
 	int		nbytes;
@@ -21,44 +21,44 @@ char	*ft_read(int fd, char **str)
 	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (fd < 0 || read(fd, buff, 0) < 0 || BUFFER_SIZE < 1)
 		return (0);
-	if (*str == NULL)
-		*str = ft_strdup("");
-	while (!(ft_strchr(*str, '\n')))
+	if (str == NULL)
+		str = ft_strdup("");
+	while (!(ft_strchr(str, '\n')))
 	{
 		if ((nbytes = read(fd, buff, BUFFER_SIZE)) < 0)
 			return (0);
 		buff[nbytes] = '\0';
-		t = ft_strjoin(*str, buff);
-		free(*str);
-		*str = t;
+		t = ft_strjoin(str, buff);
+		free(str);
+		str = t;
 		if (nbytes == 0)
 			break ;
 	}
 	free(buff);
-	return (*str);
+	return (str);
 }
 
 int		get_next_line(int fd, char **line)
 {
-	static	char	*str[1024];
+	static	char	*str;
 	char			*temp;
 	char			*t;
 
-	if (!line || !(str[fd] = ft_read(fd, &str[fd])))
+	if (!line || !(str = ft_read(fd, str)))
 		return (-1);
-	if ((temp = ft_strchr(str[fd], '\n')))
+	if ((temp = ft_strchr(str, '\n')))
 	{
-		t = str[fd];
-		*line = ft_substr(str[fd], 0, temp - str[fd]);
-		str[fd] = ft_strdup(str[fd] + (temp - str[fd]) + 1);
+		t = str;
+		*line = ft_substr(str, 0, temp - str);
+		str = ft_strdup(str + (temp - str) + 1);
 		free(t);
 		return (1);
 	}
 	else
 	{
-		*line = ft_strdup(str[fd]);
-		free(str[fd]);
-		str[fd] = NULL;
+		*line = ft_strdup(str);
+		free(str);
+		str = NULL;
 		return (0);
 	}
 }
